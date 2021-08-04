e606509c4ed9e3d6bf3dd03e48b89979bb78c506
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-08-04 18:12:49 +0600

    parsial bg added

--- src/Components/Home/Navbar/Navbar.js
+++ src/Components/Home/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light bg-transparetn">
       <div class="container-fluid">
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
@@ -17,17 +17,17 @@ const Navbar = () => {
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link  ms-5" href="/">
+              <a class="nav-link  ms-5 text-white" href="/">
                 Dental Services
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link  ms-5" href="/">
+              <a class="nav-link  ms-5 text-white" href="/">
                 Review
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link ms-5" href="/">
+              <a class="nav-link ms-5 text-white" href="/">
                 Contact Us
               </a>
             </li>
