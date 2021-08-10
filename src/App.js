ea86ab2cc0ed7d7c3f0a53d87a69075bf2bd7193
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-08-10 06:06:02 +0600

    appointment route created navbar and footer added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
+import Appointment from "./Components/Appointment/Appointment";
 import Home from "./Components/Home/Home/Home";
 
 function App() {
@@ -8,6 +9,9 @@ function App() {
       <Switch>
         <Route exact path="/">
           <Home />
+        </Route>
+        <Route path="/appointment">
+          <Appointment />
         </Route>
       </Switch>
     </Router>
---
+++ src/Components/Appointment/Appointment.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "../Shared/Footer/Footer";
+import Navbar from "../Shared/Navbar/Navbar";
+
+const Appointment = () => {
+  return (
+    <section>
+      <Navbar />
+      <Footer />
+    </section>
+  );
+};
+
+export default Appointment;
